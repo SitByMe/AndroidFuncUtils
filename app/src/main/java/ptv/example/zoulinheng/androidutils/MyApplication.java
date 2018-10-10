6c6e2bba25d456da841914c7c5b2d5ab9b470abd
SitByMe <120772813@qq.com>
2018-10-10 13:58:16 +0800

    移除SharedPreferences对Context的依赖

--- app/src/main/java/ptv/example/zoulinheng/androidutils/MyApplication.java
+++ app/src/main/java/ptv/example/zoulinheng/androidutils/MyApplication.java
@@ -6,9 +6,16 @@ import android.content.Intent;
 import ptv.example.zoulinheng.androidutils.download.DownLoadService;
 
 public class MyApplication extends Application {
+    private static MyApplication myApplication;
+
+    public static MyApplication getInstance() {
+        return myApplication;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
+        myApplication = this;
         this.startService(new Intent(this, DownLoadService.class));
     }
 }
--- app/src/main/java/ptv/example/zoulinheng/androidutils/utils/apputils/SharedPreferencesUtils.java
+++ app/src/main/java/ptv/example/zoulinheng/androidutils/utils/apputils/SharedPreferencesUtils.java
@@ -1,4 +1,4 @@
-package ptv.example.zoulinheng.androidutils.utils.apputils;
+package com.niugeyin.dcenter.utils;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -14,21 +14,20 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
+import ptv.example.zoulinheng.androidutils.MyApplication;
+
 /**
- * Created by lhZou on 2017/8/8.
+ * Created by lhZou on 2018/8/8.
  * desc: SharedPreferences工具类
  * 支持对于 List集合 与 Map集合 的存取
  */
 public class SharedPreferencesUtils {
-
     private static SharedPreferences sp;
-
     private static final String PREFERENCES_FILE_NAME = "sp_file_name";
-    public final static String SP_USER_ID = "sp_user_id";
 
-    private static SharedPreferences getSp(Context context) {
+    private static SharedPreferences getSp() {
         if (sp == null) {
-            sp = context.getSharedPreferences(PREFERENCES_FILE_NAME, Context.MODE_PRIVATE);
+            sp = MyApplication.getInstance().getApplicationContext().getSharedPreferences(PREFERENCES_FILE_NAME, Context.MODE_PRIVATE);
         }
         return sp;
     }
@@ -36,11 +35,10 @@ public class SharedPreferencesUtils {
     /**
      * 移除指定key的数据
      *
-     * @param context 上下文
-     * @param key     键
+     * @param key 键
      */
-    public static void remove(Context context, String key) {
-        SharedPreferences preferences = getSp(context);
+    public static void remove(String key) {
+        SharedPreferences preferences = getSp();
         SharedPreferences.Editor editor = preferences.edit();
         editor.remove(key);
         editor.apply();
@@ -48,11 +46,9 @@ public class SharedPreferencesUtils {
 
     /**
      * 清除所有的数据
-     *
-     * @param context 上下文
      */
-    public static void clear(Context context) {
-        SharedPreferences preferences = getSp(context);
+    public static void clear() {
+        SharedPreferences preferences = getSp();
         SharedPreferences.Editor editor = preferences.edit();
         editor.clear();
         editor.apply();
@@ -61,12 +57,11 @@ public class SharedPreferencesUtils {
     /**
      * 存入字符串
      *
-     * @param context 上下文
-     * @param key     键
-     * @param value   值
+     * @param key   键
+     * @param value 值
      */
-    public static void putString(Context context, String key, String value) {
-        SharedPreferences preferences = getSp(context);
+    public static void putString(String key, String value) {
+        SharedPreferences preferences = getSp();
         SharedPreferences.Editor editor = preferences.edit();
         editor.putString(key, value);
         editor.apply();
@@ -75,37 +70,34 @@ public class SharedPreferencesUtils {
     /**
      * 获取字符串
      *
-     * @param context 上下文
-     * @param key     键
+     * @param key 键
      * @return 得到的字符串
      */
-    public static String getString(Context context, String key) {
-        SharedPreferences preferences = getSp(context);
+    public static String getString(String key) {
+        SharedPreferences preferences = getSp();
         return preferences.getString(key, "");
     }
 
     /**
      * 获取字符串
      *
-     * @param context  上下文
      * @param key      键
      * @param defValue 默认值
      * @return 得到的字符串
      */
-    public static String getString(Context context, String key, String defValue) {
-        SharedPreferences preferences = getSp(context);
+    public static String getString(String key, String defValue) {
+        SharedPreferences preferences = getSp();
         return preferences.getString(key, defValue);
     }
 
     /**
      * 保存布尔值
      *
-     * @param context 上下文
-     * @param key     键
-     * @param value   值
+     * @param key   键
+     * @param value 值
      */
-    public static void putBoolean(Context context, String key, boolean value) {
-        SharedPreferences sp = getSp(context);
+    public static void putBoolean(String key, boolean value) {
+        SharedPreferences sp = getSp();
         SharedPreferences.Editor editor = sp.edit();
         editor.putBoolean(key, value);
         editor.apply();
@@ -114,25 +106,23 @@ public class SharedPreferencesUtils {
     /**
      * 获取布尔值
      *
-     * @param context  上下文
      * @param key      键
      * @param defValue 默认值
      * @return 返回保存的值
      */
-    public static boolean getBoolean(Context context, String key, boolean defValue) {
-        SharedPreferences sp = getSp(context);
+    public static boolean getBoolean(String key, boolean defValue) {
+        SharedPreferences sp = getSp();
         return sp.getBoolean(key, defValue);
     }
 
     /**
      * 保存long值
      *
-     * @param context 上下文
-     * @param key     键
-     * @param value   值
+     * @param key   键
+     * @param value 值
      */
-    public static void putLong(Context context, String key, long value) {
-        SharedPreferences sp = getSp(context);
+    public static void putLong(String key, long value) {
+        SharedPreferences sp = getSp();
         SharedPreferences.Editor editor = sp.edit();
         editor.putLong(key, value);
         editor.apply();
@@ -141,25 +131,23 @@ public class SharedPreferencesUtils {
     /**
      * 获取long值
      *
-     * @param context  上下文
      * @param key      键
      * @param defValue 默认值
      * @return 保存的值
      */
-    public static long getLong(Context context, String key, long defValue) {
-        SharedPreferences sp = getSp(context);
+    public static long getLong(String key, long defValue) {
+        SharedPreferences sp = getSp();
         return sp.getLong(key, defValue);
     }
 
     /**
      * 保存int值
      *
-     * @param context 上下文
-     * @param key     键
-     * @param value   值
+     * @param key   键
+     * @param value 值
      */
-    public static void putInt(Context context, String key, int value) {
-        SharedPreferences sp = getSp(context);
+    public static void putInt(String key, int value) {
+        SharedPreferences sp = getSp();
         SharedPreferences.Editor editor = sp.edit();
         editor.putInt(key, value);
         editor.apply();
@@ -168,27 +156,25 @@ public class SharedPreferencesUtils {
     /**
      * 获取int值
      *
-     * @param context  上下文
      * @param key      键
      * @param defValue 默认值
      * @return 保存的值
      */
-    public static int getInt(Context context, String key, int defValue) {
-        SharedPreferences sp = getSp(context);
+    public static int getInt(String key, int defValue) {
+        SharedPreferences sp = getSp();
         return sp.getInt(key, defValue);
     }
 
     /**
      * 保存对象
      *
-     * @param context 上下文
-     * @param key     键
-     * @param obj     要保存的对象（Serializable的子类）
-     * @param <T>     泛型定义
+     * @param key 键
+     * @param obj 要保存的对象（Serializable的子类）
+     * @param <T> 泛型定义
      */
-    public static <T extends Serializable> void putObject(Context context, String key, T obj) {
+    public static <T extends Serializable> void putObject(String key, T obj) {
         try {
-            put(context, key, obj);
+            put(key, obj);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -197,14 +183,13 @@ public class SharedPreferencesUtils {
     /**
      * 获取对象
      *
-     * @param context 上下文
-     * @param key     键
-     * @param <T>     指定泛型
+     * @param key 键
+     * @param <T> 指定泛型
      * @return 泛型对象
      */
-    public static <T extends Serializable> T getObject(Context context, String key) {
+    public static <T extends Serializable> T getObject(String key) {
         try {
-            return (T) get(context, key);
+            return (T) get(key);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -214,13 +199,12 @@ public class SharedPreferencesUtils {
     /**
      * 存储List集合
      *
-     * @param context 上下文
-     * @param key     存储的键
-     * @param list    存储的集合
+     * @param key  存储的键
+     * @param list 存储的集合
      */
-    public static void putList(Context context, String key, List<? extends Serializable> list) {
+    public static void putList(String key, List<? extends Serializable> list) {
         try {
-            put(context, key, list);
+            put(key, list);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -229,14 +213,13 @@ public class SharedPreferencesUtils {
     /**
      * 获取List集合
      *
-     * @param context 上下文
-     * @param key     键
-     * @param <E>     指定泛型
+     * @param key 键
+     * @param <E> 指定泛型
      * @return List集合
      */
-    private static <E extends Serializable> List<E> getList(Context context, String key) {
+    public static <E extends Serializable> List<E> getList(String key) {
         try {
-            return (List<E>) get(context, key);
+            return (List<E>) get(key);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -246,15 +229,14 @@ public class SharedPreferencesUtils {
     /**
      * 存储Map集合
      *
-     * @param context 上下文
-     * @param key     键
-     * @param map     存储的集合
-     * @param <K>     指定Map的键类型
-     * @param <V>     指定Map的值类型
+     * @param key 键
+     * @param map 存储的集合
+     * @param <K> 指定Map的键类型
+     * @param <V> 指定Map的值类型
      */
-    public static <K extends Serializable, V extends Serializable> void putMap(Context context, String key, Map<K, V> map) {
+    public static <K extends Serializable, V extends Serializable> void putMap(String key, Map<K, V> map) {
         try {
-            put(context, key, map);
+            put(key, map);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -263,15 +245,14 @@ public class SharedPreferencesUtils {
     /**
      * 获取Map集合
      *
-     * @param context 上下文
-     * @param key     键
-     * @param <K>     Map的键类型
-     * @param <V>     Map的值类型
+     * @param key 键
+     * @param <K> Map的键类型
+     * @param <V> Map的值类型
      * @return Map集合
      */
-    public static <K extends Serializable, V extends Serializable> Map<K, V> getMap(Context context, String key) {
+    public static <K extends Serializable, V extends Serializable> Map<K, V> getMap(String key) {
         try {
-            return (Map<K, V>) get(context, key);
+            return (Map<K, V>) get(key);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -281,18 +262,16 @@ public class SharedPreferencesUtils {
     /**
      * 存储对象
      *
-     * @param context 上下文
-     * @param key     键
-     * @param obj     对象
+     * @param key 键
+     * @param obj 对象
      * @throws IOException
      */
-    private static void put(Context context, String key, Object obj) throws IOException {
+    private static void put(String key, Object obj) throws IOException {
         if (obj == null) {
             return;
         }
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ObjectOutputStream oos = null;
-        oos = new ObjectOutputStream(baos);
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
         oos.writeObject(obj);
         // 将对象放到OutputStream中
         // 将对象转换成byte数组，并将其进行base64编码
@@ -300,21 +279,19 @@ public class SharedPreferencesUtils {
         baos.close();
         oos.close();
 
-        putString(context, key, objectStr);
+        putString(key, objectStr);
     }
 
     /**
      * 获取对象
      *
-     * @param context 上下文
-     * @param key     键
+     * @param key 键
      * @return 对象
      * @throws IOException
      * @throws ClassNotFoundException
      */
-    private static Object get(Context context, String key)
-            throws IOException, ClassNotFoundException {
-        String wordBase64 = getString(context, key);
+    private static Object get(String key) throws IOException, ClassNotFoundException {
+        String wordBase64 = getString(key);
         // 将base64格式字符串还原成byte数组
         if (TextUtils.isEmpty(wordBase64)) { //不可少，否则在下面会报java.io.StreamCorruptedException异常
             return null;
